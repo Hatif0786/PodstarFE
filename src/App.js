2db0dd841eb8b4bfcb2d5a4b0e9648369c146476
Hatif Khuld -X (hkhuld - WIPRO LIMITED at Cisco) <hkhuld@cisco.com>
2024-04-27 19:53:56 +0530

    ver1.0.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./utils/Themes";
+import Sidebar from "./components/Sidebar";
+import {Route, Router, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+  const Container = styled.div`
+    display: flex;
+    overflow-x:hidden;
+    overflow-y:hidden;
+    background: ${({ theme }) => theme.bgLight};
+    width: 100%;
+    height: 100vh;
+  `;
+
+  const Frame = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex:3;
+  `;
+
+  const [darkMode, setDarkMode] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode? darkTheme : lightTheme}>
+        <Container >
+        <BrowserRouter>
+            <Sidebar menuOpen={menuOpen} setMenuOpen={setMenuOpen} setDarkMode={setDarkMode} darkMode={darkMode}/>  
+            <Frame>
+              <Navbar setMenuOpen={setMenuOpen} menuOpen={menuOpen}/>
+              Horizon
+            </Frame>
+        </BrowserRouter>
+        </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+import {MenuRounded, PersonRounded} from "@mui/icons-material";
+import { IconButton } from '@mui/material';
+
+const NavbarDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 16px 40px;
+    align-items: center;
+    gap: 30px;
+    color: ${({theme}) => theme.text_primary}
+    background: ${({theme}) => theme.bgLight}
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(5.7px);
+    -webkit-backdrop-filter: blur(5.7px)
+    @media (max-width:768px){
+        padding:16px;
+    }
+`;
+
+const ButtonDiv = styled.div`
+    font-size:14px;
+    cursor: pointer;
+    text-decoration: none;
+    max-width: 70px;
+    align-items:center;
+    color: ${({theme}) => theme.primary};
+    border: 1px solid ${({theme}) => theme.primary};
+    display:flex;
+    border-radius:12px;
+    gap:8px;
+    padding: 8px 10px;
+`;
+
+
+const IcoButton = styled(IconButton)`
+    color: ${({theme}) => theme.text_secondary} !important;
+`;
+
+const Navbar = ({setMenuOpen, menuOpen}) => {
+  return (
+    <NavbarDiv>
+        <IcoButton onClick={() => setMenuOpen(!menuOpen)}>
+            <MenuRounded />
+        </IcoButton>
+        <ButtonDiv>
+            <PersonRounded/>
+            Login
+        </ButtonDiv>
+    </NavbarDiv>
+  )
+}
+
+export default Navbar
