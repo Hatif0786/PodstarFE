d32c3b9c6c1bce1f5916b2562184b03870840806
Hatif Khuld -X (hkhuld - WIPRO LIMITED at Cisco) <hkhuld@cisco.com>
2024-06-07 16:22:52 +0530

    ver11.2.0

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -178,7 +178,7 @@ const Dashboard = memo(({ setMenuOpened, logout, setUserlogged, setPlayerVisible
                   <div className="container-fluid text-center">
                     <div
                       className="row" id="row"
-                      style={{ marginTop: "2.5%", marginLeft: "1.5%", marginRight:"1%" }}
+                      style={{ marginTop: "2.5%", marginLeft: "1.5%", marginRight:"2.3%" }}
                     >
                       {categoryData[category].map((item, index) => (
                         <div key={index} className="col-sm-3">
