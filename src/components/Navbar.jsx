000f5d41acaf050dc139c01baaabb643bf3c7a8c
Hatif Khuld -X (hkhuld - WIPRO LIMITED at Cisco) <hkhuld@cisco.com>
2024-06-25 16:56:09 +0530

    ver11.10.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const NavbarDiv = styled.div`
   }
   @media (max-width: 576px) {
     padding-left:15px !important;
-    padding-right:8px !important;
+    margin-right:10px !important;
     gap: 7px;
   }
 `;
